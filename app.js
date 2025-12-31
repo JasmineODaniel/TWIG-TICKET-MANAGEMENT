96c61fe7405ea917b3faf29bb3bfdbc6660c3892
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2025-12-31 06:43:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.getElementById('login-form').addEventListener('submit', function(e){
+ document.getElementById('login-form').addEventListener('submit', function(e){
   e.preventDefault();
   const email = this.email.value;
   const password = this.password.value; 
